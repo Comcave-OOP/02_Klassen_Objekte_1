a9bb67217ad306a4269f9cd939da0a12c28428f8
Marinus <marinus.fehling@web.de>
2023-01-30 10:31:59 +0100

    klassen und objekte

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,6 +3,10 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
+        
+        Cat cat = new Cat();
+
+        System.out.println(cat);
      
     }
 
