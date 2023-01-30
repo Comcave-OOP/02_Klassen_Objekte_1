8234ba8dba48132d34a8b4b0b58c2fe57a11a964
Marinus <marinus.fehling@web.de>
2023-01-30 12:50:09 +0100

    this, static, void, klassen, objekte, funktionen

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,14 +4,28 @@ public class App {
     
     public static void main(String[] args) {
         
+        // 1.Instanziierung class --> object / instance
+        // Cat --> Referenztyp
+        // cat --> Referenzvariable
         Cat cat = new Cat();
+        output("Blick von aussen: " + cat);
+        // cat.tellYourAddress()
+        output(cat.tellYourAddress());
 
-        System.out.println(cat);
-     
+        output("------------------------------------------");
+
+        Cat cat2 = new Cat();
+        output("Blick von aussen: " + cat2);
+        // cat2.tellYourAddress()
+        output(cat2.tellYourAddress());
+      
     }
 
   
-
+    public static void output(String outputStr)
+    {
+        System.out.println(outputStr);
+    }
 
 
 }
--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -1,5 +1,13 @@
 package com.cc.java;
 
 public class Cat {
-    
+   
+    // public void tellYourAddress()
+    public String tellYourAddress()
+    {
+        // this --> Instanzvariable
+        // System.out.println("Blick von innen: " + this);
+        return "Blick von innen: " + this;
+    }
+
 }
